0978a04b58449677267eebf3c2acbb77cdb339d3
Austin Bingham <austin.bingham@gmail.com>
2019-12-14 13:07:31 +0100

    minor cleanup

--- subdir-lib/main.cpp
+++ subdir-lib/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <fancy/fancy.h>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    std::vector xs {1.2, 2.3, 3.4, 4.5, 5.6};
+    for (auto x: xs) {
+        std::cout << x << "\n";
+    }
     fancy::fancy_thing();
     return 0;
 }
